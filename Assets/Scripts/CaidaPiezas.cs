e6de4bd8344d9b8886ac7605b828da518a72ea4f
unknown <diego.garcia.torres@alumnos.upm.es>
2023-11-06 11:48:00 +0100

    Piezas se Paran

--- Assets/Scripts/CaidaPiezas.cs
+++ Assets/Scripts/CaidaPiezas.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class CaidaPiezas : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Rigidbody rb;
+    public Rigidbody rb;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
     // Update is called once per frame
@@ -16,13 +18,13 @@ public class CaidaPiezas : MonoBehaviour
     {
         
     }
-     private void OnCollisionEnter(Collision collision)
+    public void OnCollisionEnter(Collision collision)
     {
+        
         // Check if the object you collided with should stop your object
-        Debug.Log("Holas");
         if (collision.gameObject.CompareTag("StopTag"))
         {
-            
+            //rb.velocity = new Vector3(0,0,0);
             rb.isKinematic = true;
         }
     }
