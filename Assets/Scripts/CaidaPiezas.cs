3a09b9d9012638c895453f1e97ba390d651ba77e
GF3000 <94068411+GF3000@users.noreply.github.com>
2023-11-10 21:51:46 +0100

    pruebas piezas no se quedan atrapadas

--- Assets/Scripts/CaidaPiezas.cs
+++ Assets/Scripts/CaidaPiezas.cs
@@ -6,6 +6,7 @@ public class CaidaPiezas : MonoBehaviour
 {
     // Start is called before the first frame update
     public Rigidbody rb;
+    public LayerMask wallLayer; // Set this in the Unity editor to the layer your walls are on
 
     void Start()
     {
@@ -27,5 +28,15 @@ public class CaidaPiezas : MonoBehaviour
             //rb.velocity = new Vector3(0,0,0);
             rb.isKinematic = true;
         }
+    }
+
+    public bool isTouchingWall(Vector3 position)
+    {
+        // Check if the object is touching a object tagged as a "Pared"
+        return Physics.CheckSphere(position, 0.1f, LayerMask.GetMask("Pared"));
+
+
+        // int random = (int)Random.Range(0f, 2f);
+        // return random == 1;
     }
 }
--- Assets/Scripts/SpawnerPiezas.cs
+++ Assets/Scripts/SpawnerPiezas.cs
@@ -37,20 +37,34 @@ public class SpawnerPiezas : MonoBehaviour
     {
 
         piezaActual = Random.Range(0, piezas.Length);
+        // piezaActual = 2;
     
-        spawnPoints[0] = (int)Random.Range(-(MAX_ANCHO/2+INICIO_X), MAX_ANCHO/2+INICIO_X);
-        spawnPoints[1] = (int)Random.Range(-(MAX_ALTO/2+INICIO_Y), MAX_ALTO/2+INICIO_Y);
-                // while (piezaActual == piezaAnterior)
+       
+        Vector3 spawnPosition = newSpawnPosition();
+        piezaAnterior = piezaActual;
+        // Instantiate a pieza, if it´s touching a wall, destroy it and instantiate another one
+        // Instantiate(piezas[piezaActual], spawnPosition, Quaternion.identity);
+        // Debug.Log("Spawning " + piezas[piezaActual].name + " at " + spawnPosition + "is touching wall: " + piezas[piezaActual].GetComponent<CaidaPiezas>().isTouchingWall(spawnPosition));
+        
+        // //Print piezas[piezaActual].GetComponent<CaidaPiezas>() in the console to see if it has a CaidaPiezas component
+        // while (piezas[piezaActual].GetComponent<CaidaPiezas>().isTouchingWall(spawnPosition))
         // {
-        //     piezaActual = Random.Range(0, piezas.Length);
+        //     spawnPosition = newSpawnPosition();
         // }
 
-        Vector3 spawnPosition = new Vector3(spawnPoints[0], ALTURA, spawnPoints[1]);
-        piezaAnterior = piezaActual;
         Instantiate(piezas[piezaActual], spawnPosition, Quaternion.identity);
 
-        //Make a console message so we know it's working
-        Debug.Log("Spawning " + piezas[piezaActual].name + " at " + spawnPosition);
+ 
+    }
 
+    Vector3 newSpawnPosition()
+    {
+        int[] spawnPoints = new int[2];
+        spawnPoints[0] = (int)Random.Range(-(MAX_ANCHO/2+INICIO_X), MAX_ANCHO/2+INICIO_X);
+        spawnPoints[1] = (int)Random.Range(-(MAX_ALTO/2+INICIO_Y), MAX_ALTO/2+INICIO_Y);
+        Vector3 spawnPosition = new Vector3(spawnPoints[0], ALTURA, spawnPoints[1]);
+        return spawnPosition;
     }
 }
+
+
