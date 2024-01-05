1ba210a319e6f6da343a24c64ead050a3abab63d
Gonalgar <gonzalo.alvarez@alumnos.upm.es>
2024-01-05 23:28:39 +0100

    Aviso antes de colision

--- Assets/Scripts/CabezaScript.cs
+++ Assets/Scripts/CabezaScript.cs
@@ -7,18 +7,14 @@ public class CabezaScript : MonoBehaviour
 {
     public Transform FirstPersonController;
     public GameObject gameOverMenu;
+    public float distanciaArriba = 5f;
+    public GameObject warning;
+    public bool activa = false;
 
     // Update is called once per frame
     void Update()
     {
-        /*if (FirstPersonController != null)
-        {
-            // Establecer la posición del objeto seguidor como la posición del objeto a seguir
-            transform.position = FirstPersonController.position;
-            transform.rotation = FirstPersonController.rotation;
-            
-            transform.position = new Vector3(FirstPersonController.position.x, FirstPersonController.position.y + 0.8f, FirstPersonController.position.z);
-        }*/
+        WarningCheck();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -37,5 +33,46 @@ public class CabezaScript : MonoBehaviour
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
+    }
+    
+    private void WarningCheck()
+    {
+        Vector3 boxSize = new Vector3(0.5f, distanciaArriba, 0.5f);
+        Vector3 origin = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+        Vector3 direction = transform.TransformDirection(Vector3.up);
+
+        RaycastHit[] hits = Physics.BoxCastAll(origin, boxSize, direction, Quaternion.identity, distanciaArriba);
+
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.collider.gameObject.CompareTag("Muerte"))
+            {
+                Debug.DrawLine(origin, hit.point, Color.red);
+                warning.SetActive(true);
+                activa = true;
+            }
+            else
+            {
+                activa = false;
+            }
+        }
+        
+        if (!activa)
+        {
+            warning.SetActive(false);
+            activa = true;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        
+        Vector3 boxSize = new Vector3(0.5f, distanciaArriba, 0.5f);
+        Vector3 origin = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+        Vector3 direction = transform.TransformDirection(Vector3.up);
+
+        
+        //Gizmos.DrawWireCube(origin + direction * distanciaArriba * 0.5f, boxSize);
+        //Debug.DrawRay(origin, direction * distanciaArriba, Color.green);
     }
 }
