6ed4d6c1f9f20931ce10960015361ef384061c2f
Gonalgar <gonzalo.alvarez@alumnos.upm.es>
2024-01-07 21:21:00 +0100

    Odio mi vida

--- Assets/Scripts/CabezaScript.cs
+++ Assets/Scripts/CabezaScript.cs
@@ -7,39 +7,13 @@ public class CabezaScript : MonoBehaviour
 {
     public Transform FirstPersonController;
     public GameObject gameOverMenu;
-<<<<<<< HEAD
-=======
     public float distanciaArriba = 5f;
     public GameObject warning;
     public bool activa = false;
->>>>>>> 1ba210a319e6f6da343a24c64ead050a3abab63d
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
-        /*if (FirstPersonController != null)
-        {
-            // Establecer la posición del objeto seguidor como la posición del objeto a seguir
-            transform.position = FirstPersonController.position;
-            transform.rotation = FirstPersonController.rotation;
-            
-            transform.position = new Vector3(FirstPersonController.position.x, FirstPersonController.position.y + 0.8f, FirstPersonController.position.z);
-        }*/
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log(other.gameObject.tag);
-        if(other.gameObject.CompareTag("Muerte"))
-            Death();
-    }
-
-    public void Death()
-    {
-        if (PlayerPrefs.GetInt("Mode", 0) == 0)
-        {
-=======
         WarningCheck();
     }
 
@@ -54,7 +28,6 @@ public class CabezaScript : MonoBehaviour
     {
         if (PlayerPrefs.GetInt("Mode", 0) == 0)
         {
->>>>>>> 1ba210a319e6f6da343a24c64ead050a3abab63d
             Time.timeScale = 0f;
             gameOverMenu.SetActive(true);
             Cursor.lockState = CursorLockMode.None;
